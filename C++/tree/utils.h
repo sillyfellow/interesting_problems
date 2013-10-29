ef735e62ec1d27e2d4ac8dd0e9091814cf10ace3
sillyfellow <sandeep.nellayi@gmx.de>
2013-10-29 17:31:37 +0100

    adding binary (search) tree

---
+++ C++/tree/utils.h
@@ -0,0 +1,19 @@
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <algorithm>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> RandomArray(int lim, int range);
+
+#endif // UTIL_H 
